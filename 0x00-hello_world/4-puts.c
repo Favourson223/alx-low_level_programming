2e910f3ab50f582231e299aa97a6fac7bcea6058
Favourson <paulsmithson233@gmail.com>
2022-09-08 15:56:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-/* main point
+/**
+ * main - Entry point
  *
- *
- */
+ * Return: Always 0 (Success)
+ * /
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
