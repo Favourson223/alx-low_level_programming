3f3813e10c3502ae627a2f1beab1cb7263419167
Favourson <paulsmithson233@gmail.com>
2022-09-09 23:25:33 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ *  *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ */
+
+
+int main(void)
+
+{
+		int d;
+	
+		for (d = 1; d < 100; d++)
+		{
+			putchar((d / 10) + '0');
+			putchar((d % 10) + '0');
+			if (d != 89)
+			{
+				putchar(',');
+				putchar(' ');			
+			}		
+		}
+		putchar('\n');
+
+		return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,1 @@
-0x01-variables_if_else_whil
-
-e
+s program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative
