35ed52ffdc2e2188657de1a996f4c029239e4781
Favourson <paulsmithson233@gmail.com>
2022-09-13 03:25:24 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - program that prints _putchar, followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This repo contains solutions to solutions of 0x02-functions_nested_loops task.
