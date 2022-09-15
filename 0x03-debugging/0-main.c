cf123a22eb0150054bbf4ed9272cb16af1167827
Favourson <paulsmithson233@gmail.com>
2022-09-15 03:26:24 -0700

    0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - teats function that prints whether a function is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(1);
+
+	return (0);
+}
