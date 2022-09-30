992d5e64a318b5096872db47fbf4500e1d669ce3
Favourson <paulsmithson233@gmail.com>
2022-09-30 09:20:13 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - print the name of the executable.
- * @argc: argument count
- * @argv: argument vector
+ * main - print the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: always 0.
  */
 int main(int argc __attribute__((unused)), char **argv)
 {
-	printf("%s\n", *argv);
-	return (0);
+	printf("%s\n", *argv)
+	return (0)
 }
